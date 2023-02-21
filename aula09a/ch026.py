8eb1b805555881dcbc2533f6967f0202db294a7f
TalDoHiki <122174430+TalDoHiki@users.noreply.github.com>
2023-02-21 17:21:44 -0300

    :(

--- aula09a/ch026.py
+++ aula09a/ch026.py
@@ -1,3 +1,3 @@
 phrase = str(input('Type something: '))
 
-print('The character "A" show in a total of {} times.'.format(phrase.lower.count('a')))
+print('The character "A" show in a total of {} times, being the first time {} and the last time {}'.format(phrase.lower().count('a'), phrase.find('a'), phrase.rfind('a')))
---
+++ aula09a/ch027.py
@@ -0,0 +1,3 @@
+name = str(input('Type a fullname: '))
+
+print('The first name is: "{}". And the last name is: "{}".'.format(name.title().split()[0], name.title().split()[-1]))
