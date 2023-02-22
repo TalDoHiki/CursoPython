02f962d05d7dca76f419f2bc7bd3be0b1ddc6415
TalDoHiki <122174430+TalDoHiki@users.noreply.github.com>
2023-02-22 11:03:02 -0300

    a

--- aula09a/ch024.py
+++ aula09a/ch024.py
@@ -1,6 +1,6 @@
 name = input('Type a city name: ')
 
 if 'Santo' in name.title().split()[0]:
-    print('The city {} starts with "Santo"'.format(name))
+    print('The city {} starts with "Santo"'.format(name.title()))
 else:
-    print('The city {} do not start with "Santo'.format(name))
+    print('The city {} do not start with "Santo'.format(name.title()))
