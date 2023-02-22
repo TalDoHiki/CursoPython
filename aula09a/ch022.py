636fa907d4d2898fb890f2e60aefaead55356dd3
TalDoHiki <122174430+TalDoHiki@users.noreply.github.com>
2023-02-21 23:58:06 -0300

    :)

--- aula09a/ch022.py
+++ aula09a/ch022.py
@@ -3,6 +3,6 @@ name = str(input('Type your fullname: '))
 print(name.upper())
 print(name.lower())
 
-print(len(name.replace(' ','')))
+print('The name have a total of {} letters.'.format(len(name.replace(' ',''))))
 
-print(len(name.split()[0]))
+print('Your first name is {} and have a total of {} letters.'.format(name.split()[0],len(name.split()[0])))
--- aula09a/ch023.py
+++ aula09a/ch023.py
@@ -1,8 +1,16 @@
-n = str(input('Type a number between 0 and 9999: '))
-if int(n) > 9999 or int(n) < 0:
-    print('[ERROR]')
+n = (input('Type a number between 0 and 9999: '))
+if int(n) < 1000:
+    n = int(n)
+    u = n // 1 %10
+    d = n // 10 %10
+    c = n // 100 % 10
+    m = n // 1000 % 10
+    print('Unit: {}'.format(u))
+    print('Ten: {}'.format(d))
+    print('Hundred: {}'.format(c))
+    print('Thousand: {}'.format(m))
 else:
-    print('Unit:',n[3:])
-    print('Ten:',n[2:3])
-    print('Hundred:',n[1:2])
-    print('Thousand:', n[0:1])
+    print('Unit: {}'.format(n[3:]))
+    print('Ten: {}'.format(n[2:3]))
+    print('Hundred: {}'.format(n[1:2]))
+    print('Thousand: {}'.format(n[0:1]))
